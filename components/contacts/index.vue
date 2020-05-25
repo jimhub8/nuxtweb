045a1d85e3cfb445f94c39a281cb4d8f2cfa1c8e
Jimmy Kiarie <jimmy@localhost.localdomain>
2020-05-25 14:06:36 +0300

    commit

--- components/contacts/index.vue
+++ components/contacts/index.vue
@@ -1,7 +1,7 @@
 <template lang="en-US">
-        <v-app>
-<section class="container">
-    <div class="normal">
+<v-app>
+    <section class="container">
+        <div class="normal">
             <v-row>
                 <v-col cols="12" sm="4">
                     <v-hover v-slot:default="{ hover }" close-delay="200" open-delay="200">
@@ -51,45 +51,44 @@
                     </v-hover>
                 </v-col>
             </v-row>
-        <div class="vc_row wpb_row vc_row-fluid vc_custom_1513940636934 vc_column-gap-30 vc_row-o-content-middle vc_row-flex">
+            <div class="vc_row wpb_row vc_row-fluid vc_custom_1513940636934 vc_column-gap-30 vc_row-o-content-middle vc_row-flex">
 
-            <div class="wpb_column vc_column_container vc_col-sm-12 vc_col-has-fill">
-                <div class="vc_column-inner vc_custom_1515058776412">
-                    <div class="wpb_wrapper">
-                        <h4 style="font-size: 32px;text-align: left;color: #999" class="vc_custom_heading">Contact us</h4>
-                        <div class="vc_empty_space" style="height: 20px"><span class="vc_empty_space_inner"></span></div>
-                        <div role="form" class="wpcf7" id="wpcf7-f1260-p15-o1" lang="en-US" dir="ltr">
-                            <div class="screen-reader-response"></div>
-                            <form action="/transport-new-demos/7/contact-us/#wpcf7-f1260-p15-o1" method="post" class="wpcf7-form" novalidate="novalidate">
-                                <div style="display: none;">
-                                    <input type="hidden" name="_wpcf7" value="1260">
-                                    <input type="hidden" name="_wpcf7_version" value="4.9.2">
-                                    <input type="hidden" name="_wpcf7_locale" value="en_US">
-                                    <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f1260-p15-o1">
-                                    <input type="hidden" name="_wpcf7_container_post" value="15">
-                                </div>
-                                <div class="col-sm-12 noleftpadding">
-                                    <p><span class="wpcf7-form-control-wrap your-name"><input type="text" name="your-name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Name*"></span> </p>
-                                    <p><span class="wpcf7-form-control-wrap your-subject"><input type="text" name="your-subject" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="Subject*"></span> </p>
-                                    <p><span class="wpcf7-form-control-wrap your-email"><input type="email" name="your-email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false" placeholder="Email*"></span> </p>
-                                    <p><span class="wpcf7-form-control-wrap your-message"><textarea name="your-message" cols="25" rows="3" class="wpcf7-form-control wpcf7-textarea" aria-invalid="false" placeholder="Message*"></textarea></span> </p>
-                                    <p><input type="submit" value="SEND MESSAGE" class="wpcf7-form-control wpcf7-submit"><span class="ajax-loader"></span></p>
-                                </div>
-                                <div class="wpcf7-response-output wpcf7-display-none"></div>
-                            </form>
+                <div class="wpb_column vc_column_container vc_col-sm-12 vc_col-has-fill">
+                    <div class="vc_column-inner vc_custom_1515058776412">
+                        <div class="wpb_wrapper">
+                            <h4 style="font-size: 32px;text-align: left;color: #999" class="vc_custom_heading">Contact us</h4>
+                            <div class="vc_empty_space" style="height: 20px"><span class="vc_empty_space_inner"></span></div>
+                            <div role="form" class="wpcf7" id="wpcf7-f1260-p15-o1" lang="en-US" dir="ltr">
+                                <div class="screen-reader-response"></div>
+                                <form action="/transport-new-demos/7/contact-us/#wpcf7-f1260-p15-o1" method="post" class="wpcf7-form" novalidate="novalidate">
+                                    <div style="display: none;">
+                                        <input type="hidden" name="_wpcf7" value="1260">
+                                        <input type="hidden" name="_wpcf7_version" value="4.9.2">
+                                        <input type="hidden" name="_wpcf7_locale" value="en_US">
+                                        <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f1260-p15-o1">
+                                        <input type="hidden" name="_wpcf7_container_post" value="15">
+                                    </div>
+                                    <div class="col-sm-12 noleftpadding">
+                                        <p><span class="wpcf7-form-control-wrap your-name"><input type="text" name="your-name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Name*"></span> </p>
+                                        <p><span class="wpcf7-form-control-wrap your-subject"><input type="text" name="your-subject" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="Subject*"></span> </p>
+                                        <p><span class="wpcf7-form-control-wrap your-email"><input type="email" name="your-email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false" placeholder="Email*"></span> </p>
+                                        <p><span class="wpcf7-form-control-wrap your-message"><textarea name="your-message" cols="25" rows="3" class="wpcf7-form-control wpcf7-textarea" aria-invalid="false" placeholder="Message*"></textarea></span> </p>
+                                        <p><input type="submit" value="SEND MESSAGE" class="wpcf7-form-control wpcf7-submit"><span class="ajax-loader"></span></p>
+                                    </div>
+                                    <div class="wpcf7-response-output wpcf7-display-none"></div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1018490.5632118783!2d14.712885673277698!3d-4.320778896859434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1a6a33d66ea5b743%3A0x75996958fda47bc6!2sDesign%20Solutions%20de%20Carinata%20International!5e0!3m2!1sen!2ske!4v1590208907605!5m2!1sen!2ske" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            <div class="vc_row-full-width vc_clearfix"></div>
+
         </div>
-
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1018490.5632118783!2d14.712885673277698!3d-4.320778896859434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1a6a33d66ea5b743%3A0x75996958fda47bc6!2sDesign%20Solutions%20de%20Carinata%20International!5e0!3m2!1sen!2ske!4v1590208907605!5m2!1sen!2ske" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-        <div class="vc_row-full-width vc_clearfix"></div>
-
-    </div>
-</section>
-        </v-app>
-
+    </section>
+</v-app>
 </template>
 
 <style lang="scss">
@@ -99,7 +98,7 @@
     color: #442a5c;
 }
 
-.theme--dark.v-card {
+.theme--dark.v-card, .theme--dark.v-sheet {
     background-color: #fff;
 }
 
